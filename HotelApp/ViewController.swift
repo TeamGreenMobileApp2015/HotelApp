b9d3a72586a658ce0f3260c5d91f2630e8a3eaac
Ryan Dawkins <ryan@ryanddawkins.com>
2015-10-26 17:49:16 -0500

    First commit

---
+++ HotelApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotelApp
+//
+//  Created by Ryan Dawkins on 10/26/15.
+//  Copyright © 2015 Ryan Dawkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
