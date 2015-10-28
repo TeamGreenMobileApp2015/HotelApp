16768665995400183107b573fe05de3453fbf8c3
Ryan Dawkins <ryan@ryanddawkins.com>
2015-10-28 17:39:23 -0500

    Created createTask Storyboard

---
+++ HotelApp/DayViewViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DayViewViewController.swift
+//  HotelApp
+//
+//  Created by Ryan Dawkins on 10/28/15.
+//  Copyright © 2015 Ryan Dawkins. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DayViewViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
