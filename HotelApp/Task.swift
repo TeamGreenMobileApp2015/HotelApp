9d584f47884382bba2ad98c1544b2e9d05dda0e5
Nathaniel Hoover <Nathaniel@Nathaniels-MacBook-Pro.local>
2015-11-22 14:44:24 -0600

    Task commenting

--- HotelApp/Task.swift
+++ HotelApp/Task.swift
@@ -2,41 +2,30 @@
 //  Task.swift
 //  HotelApp
 //
-//  Created by Ryan Dawkins on 10/28/15.
-//  Copyright © 2015 Ryan Dawkins. All rights reserved.
-//
 
 import Foundation
 import Parse
 
-class Task : PFObject, PFSubclassing {
+class Task : PFObject, PFSubclassing{
 
-    
-    override class func initialize() {
-        
-        // No idea what this crap does. It was in the docs...
-        // https://parse.com/docs/ios/guide#objects-subclassing-pfobject
-        struct Static {
+    //Variables for the name of the tasks, Task due date, its department, and if its completed
+    @NSManaged var name : String
+    @NSManaged var dueDate : NSDate
+    @NSManaged var department : Department
+    @NSManaged var completed : Bool
+
+    override class func initialize(){
+        //Requred from the parse documentation
+        //https://parse.com/docs/ios/guide#objects-subclassing-pfobject
+        struct Static{
             static var onceToken : dispatch_once_t = 0;
         }
-        dispatch_once(&Static.onceToken) {
+        dispatch_once(&Static.onceToken){
             self.registerSubclass()
         }
     }
     
-    static func parseClassName() -> String {
+    static func parseClassName() -> String{
         return "Task"
     }
-    
-    // Name of the task
-    @NSManaged var name : String
-    
-    // Date the task is due
-    @NSManaged var dueDate : NSDate
-    
-    // Department to group the task to
-    @NSManaged var department : Department
-    
-    @NSManaged var completed : Bool
-    
 }
