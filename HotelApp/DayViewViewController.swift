f5b6334fb95f072244ccc43e5b80281b70c3c9d5
Jeff Shepherd <jeffmshepherd@gmail.com>
2015-11-21 20:25:06 -0600

    Started TableView for Day Task View
    I’m having  a few problems that will need to be fixed, but this is a
    start.

--- HotelApp/DayViewViewController.swift
+++ HotelApp/DayViewViewController.swift
@@ -8,12 +8,24 @@
 
 import Foundation
 import UIKit
+import Parse
 
-class DayViewViewController : UIViewController {
+class DayViewViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //@IBOutlet weak var taskTableView: UITableView!
+    @IBOutlet weak var taskTableView: UITableView!
+    
+    var taskList: [Task] = [Task]()
+    var selectedDept: String?
+    var selectedDate: NSDate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //self.taskTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "dayTaskCell")
+        
+        LoadTasks()
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -21,4 +33,65 @@ class DayViewViewController : UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("dayTaskCell", forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = taskList[indexPath.row].name
+        
+        //this gives an error everytime I try it
+        //cell.detailTextLabel?.text = taskList[indexPath.row].department.name
+
+        if taskList[indexPath.row].completed == true {
+            cell.imageView?.image = UIImage(named: "check")
+        } else {
+            cell.imageView?.image = UIImage(named: "uncheck")
+        }
+        
+        return cell
+    }
+    
+    /* 
+    //selecting rows - This isn't working and I'm not sure why.
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("Why doesn't this work?")
+    }
+    */
+    
+    func LoadTasks(){
+        if let innerQuery = Department.query() {
+            if let query = Task.query() {
+                
+                //if deparment has been selected, filter to it
+                if let dept = selectedDept {
+                    innerQuery.whereKey("name", equalTo: dept)
+                    query.whereKey("department", matchesQuery: innerQuery)
+                }
+                
+                /*
+                //The Task subclass dueDate doesn't seem to be matching up. It may have to do with it having the time as well as date.
+                if let date = selectedDate {
+                    query.whereKey("dueDate", equalTo: date)
+                }
+                */
+                
+                query.findObjectsInBackgroundWithBlock { (objects: [PFObject]?, error: NSError?) -> Void in
+                    if error == nil {
+                        if let objects = objects as? [Task] {
+                            self.taskList = objects
+                            self.taskTableView.reloadData()
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
